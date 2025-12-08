e5138c37969dc968dff53497132998b1facc6400
Abhishek Bharti <abhibharti457@gmail.com>
2025-12-09 00:32:32 +0530

    updateprofile controller and middleware cloudinary

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,8 @@ import express from "express"
 import dotenv from "dotenv"
 import {connectdb} from"./lib/db.js"
 import authroutes from "./routes/auth.route.js"
+import cookieParser from "cookie-parser";
+
 
 
 
@@ -9,6 +11,7 @@ dotenv.config()
 const app= express();
 
 app.use(express.json());
+app.use(cookieParser())
 
 
 const PORT=process.env.PORT
