ac859aa511b3edb83792bc90a83479bcc5a5fbd2
Abhishek Bharti <abhibharti457@gmail.com>
2025-12-12 16:12:00 +0530

    frontend setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      HEllo
+      <h1 className="text-4xl font-bold text-red-500">Hello Tailwind 4!</h1>
+
+
     </>
   )
 }
