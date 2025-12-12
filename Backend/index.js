a514ed2bbfb92ebcf789a22efa1121390829800e
Abhishek Bharti <abhibharti457@gmail.com>
2025-12-12 11:26:51 +0530

    model for message

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import {connectdb} from"./lib/db.js"
 import authroutes from "./routes/auth.route.js"
+
 import cookieParser from "cookie-parser";
 
 
@@ -16,6 +17,7 @@ app.use(cookieParser())
 
 const PORT=process.env.PORT
 app.use("/api/auth",authroutes)
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
