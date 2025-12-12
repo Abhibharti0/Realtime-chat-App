eb8ead5b13e38ae1a30ea1e6a1ee0c45482ed76a
Abhishek Bharti <abhibharti457@gmail.com>
2025-12-12 11:32:34 +0530

    add controller and route for message

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import {connectdb} from"./lib/db.js"
 import authroutes from "./routes/auth.route.js"
-
+import messageroutes from "./routes/message.route.js"
 import cookieParser from "cookie-parser";
 
 
@@ -17,7 +17,7 @@ app.use(cookieParser())
 
 const PORT=process.env.PORT
 app.use("/api/auth",authroutes)
-
+app.use("/api/message",messageroutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
