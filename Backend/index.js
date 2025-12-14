0db35bde485e18effb02204200286e1b1af6d823
Abhishek Bharti <abhibharti457@gmail.com>
2025-12-14 06:45:24 +0530

    vercel

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser())
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://realtime-chat-app-one-wine.vercel.app",
+    "https://realtime-chat-app-rkyz.vercel.app",
   ],
   credentials: true,
 }));
