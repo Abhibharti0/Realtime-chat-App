635df643f51f5b78da7d2ff0058ac1ceec851516
Abhishek Bharti <abhibharti457@gmail.com>
2025-12-14 06:11:43 +0530

    connect to deplayed frontend

--- Backend/index.js
+++ Backend/index.js
@@ -17,11 +17,17 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser())
 app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true,
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://realtime-chat-app-one-wine.vercel.app",
+  ],
+  credentials: true,
+}));
 
-const PORT=process.env.PORT
+
+
+const PORT=process.env.PORT||3000;
+
 app.use("/api/auth",authroutes)
 app.use("/api/messages",messageroutes)
 
